9c1f653578d1462daca4752e66b50ebccbe42fb6
Enric Calafell Oros <enric.calafell@atos.net>
2023-12-12 12:53:20 +0100

    event binding syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="app">
-      <h1>{{ message.length ? message : "Nothing here =D"}}</h1>
-      <h1 [innerText]="message.length ? message : 'Nothing here =D'"></h1>
-      <input [value]="message">
+      <h1 [innerText]="message.length ? message : 'Nothing here =D'" (click)="handleClick($event)"></h1>
+      <h1>{{ newMessage }}</h1>
+      <input [value]="message" (input)="handleInput($event)">
     </div>
   `,
   styles: [
@@ -21,8 +21,12 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class AppComponent implements OnInit {
+
+
   message!: string;
   message2: string;
+  newMessage!: string;
+
 
   constructor() {
     this.message2 = "Enric message2";
@@ -31,5 +35,16 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     console.log('Hello World!');
     this.message = "Hello World - Enric"
+  }
+
+  handleInput(event: Event) {
+    const { value } = event.target as HTMLInputElement
+    console.log("Input clicked [" + value + "]");
+    console.log("Input clicked", (event.target as HTMLInputElement).value, this, event);
+    this.newMessage = value;
+  }
+
+  handleClick(event: Event) {
+    console.log("Clicked", this, event);
   }
 }
